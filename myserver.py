664ff30a0f6e7ee87d2883db07250dbaff707766
Tete6767 <phithyaseniytechit@gmail.com>
2026-04-29 19:43:16 +0700

    Add files via upload

--- myserver.py
+++ myserver.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -8,7 +9,9 @@ def home():
     return "Server is running!"
 
 def run():
-    app.run(host='0,0,0,0',port=8080)
+    # แก้ลูกน้ำเป็นจุด (0.0.0.0) และให้ Render เป็นคนกำหนด Port อัตโนมัติ
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def server_on():
     t = Thread(target=run)
